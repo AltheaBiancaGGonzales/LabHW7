d06039816ec2ba62037344937fbaa430ba669145
Althea Gonzales <altheagonzalesctr@gmail.com>
2025-04-20 22:17:44 +0800

    Create TextDocument.java

---
+++ TextDocument.java
@@ -0,0 +1,30 @@
+public class TextDocument implements Document {
+    String filePath;
+    String encoding;
+    int wordCount;
+
+    public TextDocument() {
+        System.out.println("Creating a Text Document prototype.");
+    }
+
+    public TextDocument(String filePath, String encoding, int wordCount) {
+        this.filePath = filePath;
+        this.encoding = encoding;
+        this.wordCount = wordCount;
+    }
+
+    @Override
+    public Document clone() {
+        return new TextDocument(this.filePath, this.encoding, this.wordCount);
+    }
+
+    @Override
+    public void open() {
+        System.out.println("Opening Text Document: " + filePath + " with encoding: " + encoding + " (" + wordCount + " words)");
+    }
+
+    @Override
+    public String getType() {
+        return "Text, Path: " + filePath + ", Encoding: " + encoding + ", Words: " + wordCount;
+    }
+}
